4bcc73ed31f93742d61e31bb1d56b9b05e0e1e10
craig <craig.marvelley@boxuk.com>
2013-02-27 14:46:48 +0000

    Remove extraneous code

--- example-6/app.js
+++ example-6/app.js
@@ -47,15 +47,5 @@ Ext.application({
                 }
             ]
         });
-
-        var states = Ext.create('Ext.data.Store', {
-            fields: ['abbr', 'name'],
-            data : [
-                {"abbr":"AL", "name":"Alabama"},
-                {"abbr":"AK", "name":"Alaska"},
-                {"abbr":"AZ", "name":"Arizona"}
-                //...
-            ]
-        });
     }
 });
